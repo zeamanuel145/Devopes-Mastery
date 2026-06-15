9f5f61deaa0eb87351679d14afcb2e0c729ae4ad
zeamanuel <zeamanuel1994@gmail.com>
2026-06-15 12:37:54 +0300

    Add initial project files including CI pipeline, main application, and test script

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log('Hello!');
+console.log('This is a simple Node.js application.');
+console.log('You can run this application using Node.js on your local machine.');
+console.log('Feel free to modify this code and add more functionality!');
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log('this is a test!');
+setTimeout(() => {
+    console.log('This is a delayed message.');
+}, 2000);
+console.log('This message will appear after the delayed message.');
